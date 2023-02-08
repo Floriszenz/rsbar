1503b1afb9d6e392762caa176b22421335650635
Florian Fregien <florian.fregien@student.hpi.uni-potsdam.de>
2023-02-08 10:05:44 +0100

    build(rsbar-img): add custom build script
    add a build script to link the zbar lib for FFI access to the program

---
+++ rsbar-img/scripts/build.rs
@@ -0,0 +1,25 @@
+use std::{env, path::Path};
+
+fn main() {
+    let root = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let lib_paths = vec!["../zbar/.libs"];
+    let static_libs = vec!["zbar"];
+    let dynamic_libs = vec!["dbus-1", "jpeg", "X11", "xcb", "Xau", "Xdmcp", "systemd"];
+
+    println!("cargo:rerun-if-changed=../zbar/.libs/libzbar.a");
+
+    for path in lib_paths {
+        println!(
+            "cargo:rustc-link-search=native={}",
+            Path::new(&root).join(path).display()
+        );
+    }
+
+    for lib in static_libs {
+        println!("cargo:rustc-link-lib=static={lib}");
+    }
+
+    for lib in dynamic_libs {
+        println!("cargo:rustc-link-lib=dylib={lib}");
+    }
+}
