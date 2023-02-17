ac3eb20167dac67dd5da381df03774756db05b6c
Florian Fregien <florian.fregien@student.hpi.uni-potsdam.de>
2023-02-17 13:09:28 +0100

    refactor(rsbar-img): extract functions from scan_image

--- rsbar-img/src/utils/scan_image.rs
+++ rsbar-img/src/utils/scan_image.rs
@@ -1,4 +1,7 @@
-use std::{ffi::CStr, path::PathBuf};
+use std::{
+    ffi::CStr,
+    path::{Path, PathBuf},
+};
 
 use crate::{
     errors::{ProgramError, ProgramResult},
@@ -7,8 +10,8 @@ use crate::{
 
 use super::{cli_args::Args, XmlPrinter};
 
-const fn zbar_fourcc(a: u8, b: u8, c: u8, d: u8) -> u64 {
-    a as u64 | ((b as u64) << 8) | ((c as u64) << 16) | ((d as u64) << 24)
+const fn zbar_fourcc(code: [u8; 4]) -> u64 {
+    u32::from_le_bytes(code) as u64
 }
 
 pub fn scan_image(
@@ -17,101 +20,17 @@ pub fn scan_image(
     processor: *mut libc::c_void,
     args: &Args,
 ) -> ProgramResult<u8> {
-    let mut symbol_count: u8 = 0;
-    let image = image::open(filename)?;
+    let zimage = zbar_image_new(filename)?;
+
+    if args.xml {
+        XmlPrinter::print_source_head(filename);
+    }
+
+    process_image(processor, zimage, filename)?;
 
     unsafe {
-        let zimage = ffi::zbar_image_create();
-        assert!(!zimage.is_null());
-        ffi::zbar_image_set_format(zimage, zbar_fourcc(b'Y', b'8', b'0', b'0'));
-
-        let width = image.width();
-        let height = image.height();
-        ffi::zbar_image_set_size(zimage, width, height);
-
-        let bloblen = (width * height) as usize;
-        let blob = libc::malloc(bloblen);
-        ffi::zbar_image_set_data(zimage, blob, bloblen as u64, ffi::zbar_image_free_data);
-
-        let bytes = image.into_luma8().into_raw();
-
-        libc::memcpy(blob, bytes.as_ptr().cast(), bloblen);
-
-        if args.xml {
-            XmlPrinter::print_source_head(filename);
-        }
-
-        let processing_result = ffi::zbar_process_image(processor, zimage);
-
-        if processing_result == -1 {
-            return Err(ProgramError::ImageProcessFailed(
-                filename.display().to_string(),
-            ));
-        }
-
         // output result data
-        let mut sym = ffi::zbar_image_first_symbol(zimage);
-
-        while !sym.is_null() {
-            let typ = ffi::zbar_symbol_get_type(sym);
-
-            if typ == ZbarSymbolType::ZbarPartial {
-                continue;
-            } else if !args.xml {
-                if !args.raw {
-                    print!(
-                        "{}:",
-                        CStr::from_ptr(ffi::zbar_get_symbol_name(typ))
-                            .to_str()
-                            .unwrap()
-                    );
-                }
-
-                if args.polygon {
-                    if ffi::zbar_symbol_get_loc_size(sym) > 0 {
-                        print!(
-                            "{},{}",
-                            ffi::zbar_symbol_get_loc_x(sym, 0),
-                            ffi::zbar_symbol_get_loc_y(sym, 0)
-                        );
-                    }
-
-                    for p in 1..ffi::zbar_symbol_get_loc_size(sym) {
-                        print!(
-                            " {},{}",
-                            ffi::zbar_symbol_get_loc_x(sym, p),
-                            ffi::zbar_symbol_get_loc_y(sym, p)
-                        );
-                    }
-
-                    print!(":");
-                }
-
-                println!(
-                    "{}",
-                    CStr::from_ptr(ffi::zbar_symbol_get_data(sym))
-                        .to_str()
-                        .unwrap()
-                );
-            } else if args.xml {
-                XmlPrinter::print_index_head(idx as u8);
-                let symbol_xml = ffi::zbar_symbol_xml(sym, &mut std::ptr::null_mut(), &mut 0);
-
-                if let Ok(symbol_xml) = CStr::from_ptr(symbol_xml).to_str() {
-                    XmlPrinter::print_symbol(symbol_xml);
-                }
-
-                XmlPrinter::print_index_foot();
-            }
-
-            symbol_count += 1;
-
-            if args.oneshot {
-                break;
-            }
-
-            sym = ffi::zbar_symbol_next(sym);
-        }
+        let symbol_count = output_result(zimage, args, idx);
 
         ffi::zbar_image_destroy(zimage);
 
@@ -126,7 +45,121 @@ pub fn scan_image(
         if args.xml {
             XmlPrinter::print_source_foot();
         }
+
+        Ok(symbol_count)
+    }
+}
+
+fn zbar_image_new(filename: &PathBuf) -> ProgramResult<*mut libc::c_void> {
+    let image = image::open(filename)?;
+
+    unsafe {
+        let zimage = ffi::zbar_image_create();
+        assert!(!zimage.is_null());
+
+        ffi::zbar_image_set_format(zimage, zbar_fourcc([b'Y', b'8', b'0', b'0']));
+
+        let width = image.width();
+        let height = image.height();
+        ffi::zbar_image_set_size(zimage, width, height);
+
+        let bloblen = (width * height) as usize;
+        let blob = libc::malloc(bloblen);
+        ffi::zbar_image_set_data(zimage, blob, bloblen as u64, ffi::zbar_image_free_data);
+
+        let bytes = image.into_luma8().into_raw();
+
+        libc::memcpy(blob, bytes.as_ptr().cast(), bloblen);
+
+        Ok(zimage)
+    }
+}
+
+fn process_image(
+    processor: *mut libc::c_void,
+    zimage: *mut libc::c_void,
+    filename: &Path,
+) -> ProgramResult<()> {
+    unsafe {
+        let processing_result = ffi::zbar_process_image(processor, zimage);
+
+        if processing_result == -1 {
+            return Err(ProgramError::ImageProcessFailed(
+                filename.display().to_string(),
+            ));
+        }
     }
 
-    Ok(symbol_count)
+    Ok(())
+}
+
+fn output_result(zimage: *mut libc::c_void, args: &Args, idx: usize) -> u8 {
+    let mut symbol_count: u8 = 0;
+
+    unsafe {
+        let mut symbol = ffi::zbar_image_first_symbol(zimage);
+
+        while !symbol.is_null() {
+            let symbol_type = ffi::zbar_symbol_get_type(symbol);
+
+            if symbol_type == ZbarSymbolType::ZbarPartial {
+                continue;
+            } else if !args.xml {
+                if !args.raw {
+                    print!(
+                        "{}:",
+                        CStr::from_ptr(ffi::zbar_get_symbol_name(symbol_type))
+                            .to_str()
+                            .unwrap()
+                    );
+                }
+
+                if args.polygon {
+                    if ffi::zbar_symbol_get_loc_size(symbol) > 0 {
+                        print!(
+                            "{},{}",
+                            ffi::zbar_symbol_get_loc_x(symbol, 0),
+                            ffi::zbar_symbol_get_loc_y(symbol, 0)
+                        );
+                    }
+
+                    for p in 1..ffi::zbar_symbol_get_loc_size(symbol) {
+                        print!(
+                            " {},{}",
+                            ffi::zbar_symbol_get_loc_x(symbol, p),
+                            ffi::zbar_symbol_get_loc_y(symbol, p)
+                        );
+                    }
+
+                    print!(":");
+                }
+
+                println!(
+                    "{}",
+                    CStr::from_ptr(ffi::zbar_symbol_get_data(symbol))
+                        .to_str()
+                        .unwrap()
+                );
+            } else if args.xml {
+                XmlPrinter::print_index_head(idx as u8);
+                let symbol_xml = ffi::zbar_symbol_xml(symbol, &mut std::ptr::null_mut(), &mut 0);
+
+                if let Ok(symbol_xml) = CStr::from_ptr(symbol_xml).to_str() {
+                    XmlPrinter::print_symbol(symbol_xml);
+                }
+
+                XmlPrinter::print_index_foot();
+            }
+
+            symbol_count += 1;
+
+            if args.oneshot {
+                break;
+            }
+
+            symbol = ffi::zbar_symbol_next(symbol);
+        }
+    }
+
+    symbol_count
 }
